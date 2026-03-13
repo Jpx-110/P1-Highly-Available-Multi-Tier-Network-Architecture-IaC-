80559464a82da7dba856d9c5a60cf97b8f2f8627
Jamil <jamil@Jamils-MacBook-Pro.local>
2026-03-13 23:54:15 +0000

    feat: finalized full architecture with NAT gateway and bastion

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-#2. RESOURCE: Creare a VPC with CIDR Block - this is the network perimter for our infrastructure
+# --- 2. VPC Creare a VPC with CIDR Block - this is the network perimter for our infrastructure
 resource "aws_vpc" "main_vpc" {
     cidr_block              = "10.0.0.0/16"
     enable_dns_hostnames    = true
     tags = { Name = "P1-Highly Available Multi Tier Network Architecture" } 
 }
 
-#3.RESOURCE > Subnets > Public(Internet facing) and Private(secure) subnets
+# --- 3. Subnets > Public(Internet facing) and Private(secure) subnets
 resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = "10.0.1.0/24"
@@ -24,12 +24,12 @@ resource "aws_subnet" "private" {
   tags = { Name = "Private-Subnet" }
 }
 
-# 4. INTERNET GATEWAY: The entry point
+# --- 4. INTERNET GATEWAY: The entry point
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_vpc.id
 }
 
-# 5. ROUTE TABLE: Rules to send public traffic to the Gateway
+# --- 5. ROUTE TABLE: Rules to send public traffic to the Gateway
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
   route {
@@ -41,4 +41,75 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+# --- 6. SECURITY GROUP: checks all network data/packets trying to access servers
+resource "aws_security_group" "web_sg" {
+  name        = "Web-Security-Group"
+  description = "Allow HTTPS inbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  # Inbound Rule: Allow HTTPS (Port 443) from anywhere
+  ingress {
+    description = "Allow Secure Web Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # The whole internet
+  }
+
+  # Outbound Rule: Allow everything OUT
+  # This allows the server to fetch updates or talk to the database
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Represents ALL protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "Web-SG" }
+}
+
+
+
+# --- 7. NAT GATEWAY (The "One-Way Mirror") ---
+# Allows private resources to reach out for updates, but prevents the public traffic from internet reaching in.
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id # NAT Gateway MUST live in the Public Subnet
+  tags          = { Name = "Main-NAT-Gateway" }
+}
+
+# --- 8. PRIVATE ROUTE TABLE ---
+# Tells the Private Subnet: "If you need the internet, go through the NAT Gateway."
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# --- 9. BASTION HOST (The "Jump Box server") ---
+# A secure entry point in the Public Subnet for Admin access. Think of like a side doorway to a bank vault for authorised personnel to acces without going through front reception
+resource "aws_instance" "bastion_host" {
+  ami           = "ami-0e8d228ad90af673b" # Amazon Linux 2023 AMI for London
+  instance_type = "t2.micro"             # Free Tier eligible
+  subnet_id     = aws_subnet.public.id
+  
+  # Attach the Security Group we built earlier
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "WMP-Bastion-Host"
+  }
 }
